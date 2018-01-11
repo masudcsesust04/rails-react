596764241b0f1ac8dea9cb1fbce40b31df6bc734
Md. Masud Rana <masudcsesust04@gmail.com>
2018-01-11 15:14:56 +0600

    Added twitter bootstrap and updated style of the components

--- app/javascript/packs/components/Header.jsx
+++ app/javascript/packs/components/Header.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <nav className="navbar navbar-default">
+  <nav className="navbar navbar-inverse navbar-fixed-top">
     <div className="container">
       <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <a className="navbar-brand" href="/">React Demo</a>
+      </div>
+      <div id="navbar" className="collapse navbar-collapse">
         <ul className="nav navbar-nav">
-          <li><Link to="/" href>Home</Link></li>
-          <li><Link to="/users" href>User</Link></li>
+          <li><Link to="/users" href>Users</Link></li>
+          <li><Link to="/diamonds" href>Diamonds</Link></li>
           <li><Link to="/games" href>Game</Link></li>
         </ul>
       </div>
